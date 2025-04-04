3dd58811a8805a16f7af396ef95f4d75f2380abe
Antony Antony <antony.antony@secunet.com>
2025-04-04 07:22:21 +0200

    mk/yang.mk sync up the mk/yang.mk file

--- mk/yang.mk
+++ mk/yang.mk
@@ -3,18 +3,23 @@ VERSION := $(shell sed -e '/^\#+RFC_VERSION:/!d;s/\#+RFC_VERSION: *\([0-9]*\)/\1
 VERSION_NOZERO := $(shell echo "$(VERSION)" | sed -e 's/^0*//')
 NEXT_VERSION := $(shell printf "%02d" "$$(($(VERSION_NOZERO) + 1))")
 PREV_VERSION := $(shell printf "%02d" "$$(($(VERSION_NOZERO) - 1))")
-DTYPE := $(word 2,$(subst -, ,$(BASE)))
-PBRANCH := publish-$(DTYPE)-$(VERSION)
+SBASE := $(patsubst draft-%,%,$(BASE))
+#DTYPE := $(word 2,$(subst -, ,$(BASE)))
+PBRANCH ?= publish-$(SBASE)-$(VERSION)
 PBASE := publish/$(BASE)-$(VERSION)
 VBASE := draft/$(BASE)-$(VERSION)
 LBASE := draft/$(BASE)-latest
 SHELL := /bin/bash
+MAIN_BRANCH ?= main #older repos set to master
+PUSH_TO_REMOTE ?= origin
 
 # If you have docker you can avoid having to install anything by leaving this.
 ifeq ($(CIRCLECI),)
 export DOCKRUN ?= docker run --user $(shell id -u) --network=host -v $$(pwd):/work labn/org-rfc
 endif
 EMACSCMD := $(DOCKRUN) emacs -Q --batch --debug-init --eval '(setq-default indent-tabs-mode nil)' --eval '(setq org-confirm-babel-evaluate nil)' -l ./ox-rfc.el
+
+BRANCH_EXISTS := $(shell git rev-parse --verify $(MAIN_BRANCH) 2>/dev/null)
 
 all: $(LBASE).xml $(LBASE).txt $(LBASE).html # $(LBASE).pdf
 
@@ -25,27 +30,44 @@ git-clean-check:
 	@echo Checking for git clean status
 	@STATUS="$$(git status -s)"; [[ -z "$$STATUS" ]] || echo "$$STATUS"
 
+push_to_remote:
+ifeq ($(strip $(PUSH_TO_REMOTE)),)
+		@echo "PUSH_TO_REMOTE is not set. Skipping git push."
+else
+		git push $(PUSH_TO_REMOTE) $(PBRANCH)
+		git push $(PUSH_TO_REMOTE) -f --tags
+endif
+
+
+.PHONY: main-branch-check
+main-branch-check:
+ifeq ($(BRANCH_EXISTS),)
+    $(error Branch '$(MAIN_BRANCH)' does not exist. Exiting.)
+endif
+
 .PHONY: publish
-publish: git-clean-check $(VBASE).xml $(VBASE).txt $(VBASE).html
+publish: main-branch-check git-clean-check $(VBASE).xml $(VBASE).txt $(VBASE).html
 	if [ -f $(PBASE).xml ]; then echo "$(PBASE).xml already present, increment version?"; exit 1; fi
+	@mkdir -p publish
 	cp $(VBASE).xml $(VBASE).txt $(VBASE).html publish
 	git checkout -b $(PBRANCH)
-	git tag -m "yank.mk publish-$(DTYPE)-$(VERSION)" bp-$(PBRANCH)
-	git push -f --tags
+	git tag -m "yank.mk: publish-$(SBASE)-$(VERSION)" bp-$(PBRANCH)
 	git add $(PBASE).xml $(PBASE).txt $(PBASE).html
-	git commit -m "yank.mk publish-$(DTYPE)-$(VERSION)"
-	git push origin $(PBRANCH)
-	git checkout main
+	git commit -m "yank.mk: publish-$(SBASE)-$(VERSION)"
+	git tag -m "yank.mk: published-$(SBASE)-$(VERSION)" published-$(SBASE)-$(VERSION)
+	$(MAKE) PBRANCH=$(PBRANCH) push_to_remote
+	git checkout $(MAIN_BRANCH)
 	git merge --ff-only $(PBRANCH)
 	sed -i -e 's/\#+RFC_VERSION: *\([0-9]*\)/\#+RFC_VERSION: $(NEXT_VERSION)/' $(ORG)
-	git commit -am "yank.mk new version -$(NEXT_VERSION) post-publish"
+	git commit -am "yank.mk: new version -$(NEXT_VERSION) post-publish $(SBASE)-$(VERSION)"
+	$(MAKE) PBRANCH=$(MAIN_BRANCH) push_to_remote
 
 #republish:
 #	sed -i -e 's/\#+RFC_VERSION: *\([0-9]*\)/\#+RFC_VERSION: $(PREV_VERSION)/' $(ORG)
 #	cp $(VBASE).xml $(VBASE).txt $(VBASE).html publish
 #	git add $(PBASE).xml $(PBASE).txt $(PBASE).html
-#	git commit -m "publish-$(DTYPE)-$(VERSION)-update"
-#	git tag -a -f -m "yank.mk publish-$(DTYPE)-$(VERSION) update" publish-$(DTYPE)-$(VERSION)
+#	git commit -m "publish-$(SBASE)-$(VERSION)-update"
+#	git tag -a -f -m "yank.mk publish-$(SBASE)-$(VERSION) update" publish-$(SBASE)-$(VERSION)
 #	sed -i -e 's/\#+RFC_VERSION: *\([0-9]*\)/\#+RFC_VERSION: $(VERSION)/' $(ORG)
 
 draft:
